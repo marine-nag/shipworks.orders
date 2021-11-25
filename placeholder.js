4024ccf8d544bd1955f1dae5c220f22a9c07efe2
marine-nag <71370369+marine-nag@users.noreply.github.com>
2021-11-25 10:56:39 +0200

    Create placeholder.js

---
+++ placeholder.js
@@ -0,0 +1,52 @@
+"use strict"
+
+define(function (require) {
+    const placeholderManager = require("core/placeholderManager");
+    const ngComponent = require("core/ngComponent");
+
+    const pdfMake = require('https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/pdfmake.js');
+    const pdfFonts = require('https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/vfs_fonts.js');
+    const bwipjs = require('https://cdnjs.cloudflare.com/ajax/libs/bwip-js/3.0.0/bwip-js.js');
+
+
+    var docDefinition;
+
+    var placeHolder = function ($scope, $element, controlService) {
+
+        //const _this = this;
+        this.getItems = () => {
+            var items = [{
+                text: "Move to ShipWorks",  // Button name
+                key: "placeholderPrintInvoiceTemplate",  // Button id (unique)
+                icon: "fa fa-print",  // Button icon
+                content: {
+                    moduleName: "placeholderShipWorksTemplate",
+                    controlName: "placeholderShipWorksTemplate"
+                }
+            }];
+
+            return items;
+        };
+
+        this.isEnabled = (itemKey) => {
+            return true;
+        };
+
+        this.onClick = () => {
+            var orders = $scope.viewStats.get_selected_orders();
+
+            if (orders.length < 1) {
+                alert('Please select at least one order');
+                return;
+            }
+
+            //$scope.getOrderDataBySomeID();
+        };
+
+       
+        };
+      
+
+    placeholderManager.register("OpenOrders_OrderControlButtons", placeHolder);
+
+});
