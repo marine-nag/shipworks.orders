3763010047e80ccc1b2cb91d8ff57bade8dc5745
marine-nag <71370369+marine-nag@users.noreply.github.com>
2021-11-29 15:10:38 +0200

    Update placeholder.js

--- placeholder.js
+++ placeholder.js
@@ -51,7 +51,32 @@ define(function (require) {
             var requestBody = { orderIDs: ids };
             var url = "https://cf62-46-119-185-221.ngrok.io/Order/addOrdersToShipWorks";
             
-            
+            const dataToSend = JSON.stringify(requestBody);
+            let dataReceived = ""; 
+            fetch("", {
+                credentials: "same-origin",
+                mode: "same-origin",
+                method: "post",
+                headers: { "Content-Type": "application/json" },
+                body: dataToSend
+            })
+                .then(resp => {
+                    if (resp.status === 200) {
+                        return resp.json()
+                    } else {
+                        console.log("Status: " + resp.status)
+                        return Promise.reject("server")
+                    }
+                })
+                .then(dataJson => {
+                    dataReceived = JSON.parse(dataJson)
+                })
+                .catch(err => {
+                    if (err === "server") return
+                    console.log(err)
+                })
+
+            console.log(`Received: ${dataReceived}`);    
 
             //$scope.getOrderDataBySomeID();
         };
